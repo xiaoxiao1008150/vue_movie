b2573f52a2b84b6f15e9cb70e67562656306114b
Cui <cuixiao@gmail.com>
2017-11-15 21:35:56 +0800

    update baseurl

--- src/api/movie.js
+++ src/api/movie.js
@@ -1,4 +1,8 @@
 import http from './axios'
+let baseUrl = 'http://localhost:8080'
+if (process.env.NODE_ENV === 'production') {
+  baseUrl = 'http://47.104.98.140:8000'
+}
 
 let API_KEY = 'apikey=0b2bdeda43b5688921839c8ecb20399b'
 function helpUrl (query) {
@@ -11,29 +15,26 @@ function helpUrl (query) {
 
 // 搜索电影 根据关键词 电影名 人物
 export const _getInfo = (key, query, params) => {
-  console.log('url', `/api/movie/search?q=${key}&` + helpUrl(query))
-  return http.fetchGet(`/api/movie/search?q=${key}&` + helpUrl(query), params)
+  return http.fetchGet(baseUrl + `/api/movie/search?q=${key}&` + helpUrl(query), params)
 }
 // 获取全即将上映电影 默认20条数据
 export const _getComingSoon = (query, params) => {
-  // console.log('coming===', `/api/movie/coming_soon?` + helpUrl(query))
-  return http.fetchGet(`/api/movie/coming_soon?` + helpUrl(query), params)
+  return http.fetchGet(baseUrl + `/api/movie/coming_soon?` + helpUrl(query), params)
 }
 // 正在热映
 export const _getHot = (query, params) => {
-  // console.log('hot===', `/api/movie/in_theaters?` + helpUrl(query))
-  return http.fetchGet(`/api/movie/in_theaters?` + helpUrl(query), params)
+  return http.fetchGet(baseUrl + `/api/movie/in_theaters?` + helpUrl(query), params)
 }
 
 // 获取一个电影详细信息
 export const _getSingleMovie = (id, params) => {
-  return http.fetchGet(`/api/movie/subject/${id}?` + API_KEY, params)
+  return http.fetchGet(baseUrl + `/api/movie/subject/${id}?` + API_KEY, params)
 }
 // 获取一个电影的剧照
 export const _getMoviePhotoes = (id, params) => {
-  return http.fetchGet(`/api/movie/subject/${id}/photos?` + helpUrl(), params)
+  return http.fetchGet(baseUrl + `/api/movie/subject/${id}/photos?` + helpUrl(), params)
 }
 // 获取一个电影的短评
 export const _getMovieReviews = (id, query, params) => {
-  return http.fetchGet(`/api/movie/subject/${id}/reviews?` + helpUrl(query), params)
+  return http.fetchGet(baseUrl + `/api/movie/subject/${id}/reviews?` + helpUrl(query), params)
 }
